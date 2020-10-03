0cac02182b6b0ebe02d7f583094cc4fe3491245b
Subrato Ghosh <subrato2393@gmail.com>
2020-10-04 00:32:00 +0600

    add generics code

---
+++ CSharpBasicPractise/GenericsApp/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace GenericsApp
+{
+    public class Calculator<T>
+    {
+        public T FirstNumber { get; set; }
+        public T SecondNumber { get; set; }
+
+        public Calculator(T firstNumber,T secondNumber)
+        {
+            this.FirstNumber = firstNumber;
+            this.SecondNumber = secondNumber;
+        }
+        public T Add<T>(T firstNumber,T secondNumber)
+        {
+            dynamic a = firstNumber;
+            dynamic b = secondNumber;
+            return a + b;
+        }
+        public void Multiply<T>(T firstNumber, T secondNumber)
+        {
+            dynamic a = firstNumber;
+            dynamic b = secondNumber;
+            Console.WriteLine("Multiplication = "+(a+b));
+        }
+    }
+}
---
+++ CSharpBasicPractise/GenericsApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GenericsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator<int> aCalculator = new Calculator<int>(34,89);
+
+            var addition = aCalculator.Add<int>(10, 21);
+            aCalculator.Multiply<double>(3.99, 9.6);   
+            
+            Console.WriteLine("addition "+addition);
+          
+            
+            Console.ReadLine();
+
+        }
+    }
+}
