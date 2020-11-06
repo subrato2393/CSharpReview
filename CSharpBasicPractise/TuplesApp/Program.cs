6205528dc5f8052d0a87baf0e85a909eec30b45a
Subrato <Subrato@DEV-LAP-01>
2020-11-06 10:18:33 +0600

    adding tuple practise using C#.Net 7.0

--- CSharpBasicPractise/TuplesApp/Program.cs
+++ CSharpBasicPractise/TuplesApp/Program.cs
@@ -4,15 +4,22 @@ namespace TuplesApp
 {
     class Program
     {
+        //Tuples before c#.net 7
+        //static void Main(string[] args)
+        //{
+        //    TuplesDemo tuplesDemo = new TuplesDemo();
+        //    Tuple<int,int,double> t= tuplesDemo.Math(10, 4);
+        //    Console.WriteLine($"Addition {t.Item1}  Multiplication {t.Item2} Division {t.Item3}" );
+        //    Console.ReadLine();
+        //}
+
+        //Tuples in C#.Net 7.0
         static void Main(string[] args)
         {
             TuplesDemo tuplesDemo = new TuplesDemo();
-            Tuple<int,int,double> t= tuplesDemo.Math(10, 4);
-            Console.WriteLine($"Addition {t.Item1}  Multiplication {t.Item2} Division {t.Item3}" );
+            var  t = tuplesDemo.Math(10, 4);
+            Console.WriteLine($"Addition {t.add}  Multiplication {t.mul} Division {t.div}");
             Console.ReadLine();
-        }
-
-        //Return Two Value from a method
-        
+        }  
     }
 }
--- CSharpBasicPractise/TuplesApp/TuplesDemo.cs
+++ CSharpBasicPractise/TuplesApp/TuplesDemo.cs
@@ -6,12 +6,24 @@ namespace TuplesApp
 {
     public class TuplesDemo 
     {
-       public Tuple<int, int, double> Math(int a, int b)
+        //Tuples before C# 7
+
+        //public Tuple<int, int, double> Math(int a, int b)
+        // {
+        //     int add = a + b;
+        //     int multiply = a * b;
+        //     double division = a / b;
+        //     Tuple<int, int, double> t = Tuple.Create<int, int, double>(add, multiply, division);
+        //     return t;
+        // }  
+
+        //Tuples in C#.NET 7.0
+        public (int add, int mul, double div) Math(int a, int b)
         {
             int add = a + b;
             int multiply = a * b;
             double division = a / b;
-            Tuple<int, int, double> t = Tuple.Create<int, int, double>(add, multiply, division);
+            var t = (add, multiply, division);
             return t;
         }
     }
