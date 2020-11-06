eb594477fb649876bcb3ceccf27f5f9887e1110e
Subrato <Subrato@DEV-LAP-01>
2020-11-06 10:07:21 +0600

    adding tuples practise before C#.NET 7

---
+++ CSharpBasicPractise/TuplesApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TuplesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TuplesDemo tuplesDemo = new TuplesDemo();
+            Tuple<int,int,double> t= tuplesDemo.Math(10, 4);
+            Console.WriteLine($"Addition {t.Item1}  Multiplication {t.Item2} Division {t.Item3}" );
+            Console.ReadLine();
+        }
+
+        //Return Two Value from a method
+        
+    }
+}
---
+++ CSharpBasicPractise/TuplesApp/TuplesDemo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TuplesApp
+{
+    public class TuplesDemo 
+    {
+       public Tuple<int, int, double> Math(int a, int b)
+        {
+            int add = a + b;
+            int multiply = a * b;
+            double division = a / b;
+            Tuple<int, int, double> t = Tuple.Create<int, int, double>(add, multiply, division);
+            return t;
+        }
+    }
+}
