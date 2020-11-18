d3dae83734a66c140fb9fbb08ac81381605132bb
Subrato <Subrato@DEV-LAP-01>
2020-11-18 17:20:15 +0600

    adding file system read/write

--- CSharpBasicPractise/FileHandlingApp/Program.cs
+++ CSharpBasicPractise/FileHandlingApp/Program.cs
@@ -13,18 +13,35 @@ namespace FileHandlingApp
 
             //Input value
             string item1 = "name:subroto,Email:s@gmail.com";
-            string item2 = "phone:01283838,address:dhaka";
-            
+            string item2 = "name:subro,Email:sh@gmail.com";
+            string item3 = "name:suvo,Email:sv@gmail.com";
+
+            List<Person> persons = new List<Person>()
+            {
+                new Person(){FirstName="subroto",LastName="ghosh",Email="s@gmail.com"},
+                new Person(){FirstName="subro",LastName="ghosh",Email="k@gmail.com"},
+                new Person(){FirstName="suvo",LastName="ghosh",Email="y@gmail.com"},
+            };
             //file path
             string filePath = @"E:\DST\text.txt";
-           
+            List<string> strList = new List<string>();
+            foreach (var item in persons)
+            {
+                strList.Add("First Name :"+item.FirstName+" "+"Last Name : "+item.LastName+" "+"Email :"+item.Email);
+            }
+            //foreach (var item in strList)
+            //{
+                
+            //}
+
             //Add value in string
-            List<string> list = new List<string>();
-            list.Add(item1);
-            list.Add(item2);
+            //List<string> list = new List<string>();
+            //list.Add(item1);
+            //list.Add(item2);
+            //list.Add(item3);
 
             //Write file
-            File.WriteAllLines(filePath, list);
+            File.WriteAllLines(filePath, strList);
             Console.WriteLine("......File Write Successfully.......\n");
 
             //Read From a file
@@ -38,8 +55,6 @@ namespace FileHandlingApp
             {
                 Console.WriteLine(item);
             }
-
-
             Console.ReadLine();
         }
     }
