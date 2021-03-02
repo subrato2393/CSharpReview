df82a17ecb0b1dc9d1b9d86024d370097443b76e
Subrato Ghosh <subrato2393@gmail.com>
2021-03-02 13:40:34 +0600

    Asyncronus operation using Task.Run() operation.

--- CSharpBasicPractise/TaskAsyncApp/Program.cs
+++ CSharpBasicPractise/TaskAsyncApp/Program.cs
@@ -8,42 +8,51 @@ namespace TaskAsyncApp
     {
         static void Main(string[] args)
         {
-            Coffe coffe = MakeCoffe();
-            Console.WriteLine("Coffe is ready");
+            Console.WriteLine($"Thread id {Thread.CurrentThread.ManagedThreadId}");
+        
+            Task task = Task.Run(() =>
+            {
+                Coffe coffe = MakeCoffe();
+                Console.WriteLine("Coffe is ready");
+            });
 
-            Console.WriteLine("----------------->>");
+            Task task1 = Task.Run(() =>
+            {
+                Egg egg = FryEgg();
+                Console.WriteLine("Egg is ready to eat");
 
-            Egg egg = FryEgg();
-            Console.WriteLine("Egg is ready to eat");
+            });
 
-            Console.WriteLine("----------------->>");
-
-            Chicken chicken = WarmUpChicken();
-            Console.WriteLine("Chicken is ready");
-
-            Console.WriteLine("----------------->>");
+            Task task2 = Task.Run(() =>
+            {
+                Chicken chicken = WarmUpChicken();
+                Console.WriteLine("Chicken is ready");
+            });
 
             Console.WriteLine("Your breakfirst is Ready to eat");
             Console.ReadLine();
         }
         private static Chicken WarmUpChicken()
         {
+            Console.WriteLine($"Thread id {Thread.CurrentThread.ManagedThreadId}");
             Console.WriteLine("Put chicken on heater");
-            Task.Delay(2000).Wait();
+            //Task.Delay(2000).Wait();
             Console.WriteLine("Heat Chicken");
             return new Chicken();
         }
-        private static Egg FryEgg()
+        private  static Egg FryEgg()
         {
+            Console.WriteLine($"Thread id {Thread.CurrentThread.ManagedThreadId}");
             Console.WriteLine("Warming the egg pan and oil");
-            Task.Delay(2000).Wait();
+           // Task.Delay(2000).Wait();
             Console.WriteLine("Fry Egg");
-            return new Egg();
+            return new Egg(); 
         }
-        private static Coffe MakeCoffe()
+        private  static Coffe MakeCoffe()
         {
+            Console.WriteLine($"Thread id {Thread.CurrentThread.ManagedThreadId}");
             Console.WriteLine("Heat water for coffe");
-            Task.Delay(2000).Wait();
+            //Task.Delay(2000).Wait();
             Console.WriteLine("Mix coffe and sugar to water when water warm");
             return new Coffe();
         }
