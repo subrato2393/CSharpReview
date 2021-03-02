d6e4093daa0ac40b392733e932f5836e2908387e
Subrato Ghosh <subrato2393@gmail.com>
2021-03-02 11:42:16 +0600

    using task.delay instead of Thread.,sleep

--- CSharpBasicPractise/TaskAsyncApp/Program.cs
+++ CSharpBasicPractise/TaskAsyncApp/Program.cs
@@ -10,38 +10,40 @@ namespace TaskAsyncApp
         {
             Coffe coffe = MakeCoffe();
             Console.WriteLine("Coffe is ready");
-           
+
             Console.WriteLine("----------------->>");
-            
+
             Egg egg = FryEgg();
             Console.WriteLine("Egg is ready to eat");
 
             Console.WriteLine("----------------->>");
-           
+
             Chicken chicken = WarmUpChicken();
             Console.WriteLine("Chicken is ready");
 
             Console.WriteLine("----------------->>");
-          
+
             Console.WriteLine("Your breakfirst is Ready to eat");
             Console.ReadLine();
         }
         private static Chicken WarmUpChicken()
         {
             Console.WriteLine("Put chicken on heater");
+            Task.Delay(2000).Wait();
             Console.WriteLine("Heat Chicken");
             return new Chicken();
         }
         private static Egg FryEgg()
         {
             Console.WriteLine("Warming the egg pan and oil");
+            Task.Delay(2000).Wait();
             Console.WriteLine("Fry Egg");
             return new Egg();
         }
         private static Coffe MakeCoffe()
         {
             Console.WriteLine("Heat water for coffe");
-            //Thread.Sleep(2000);
+            Task.Delay(2000).Wait();
             Console.WriteLine("Mix coffe and sugar to water when water warm");
             return new Coffe();
         }
